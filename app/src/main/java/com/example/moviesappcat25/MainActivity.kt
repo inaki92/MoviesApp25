d920ea527d2f0d72d9c9a1a05c2c2fab3ae69cd5
inaki <ironhide0905@gmail.com>
2022-06-30 11:58:44 -0400

    creating  navigation

--- app/src/main/java/com/example/moviesappcat25/MainActivity.kt
+++ app/src/main/java/com/example/moviesappcat25/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.moviesappcat25
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.moviesappcat25.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_container)
+        val appConfig = AppBarConfiguration(
+            setOf(R.id.PayingNowFragment, R.id.UpcomingFragment, R.id.PopularFragment)
+        )
+
+        setupActionBarWithNavController(navController, appConfig)
+        binding.navigationBar.setupWithNavController(navController)
     }
 }
